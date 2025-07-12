a19ee8d05391ae9408ce79042b9085a2e3ce45b0
krushilkdesai <krushildesai2005@gmail.com>
2025-07-12 14:48:03 +0530

    update login

--- skill_swap/account/views.py
+++ skill_swap/account/views.py
@@ -31,13 +31,13 @@ def logoutaccount(request):
 
 def loginaccount(request):
     if request.method == 'GET':
-        return render(request, 'loginaccount.html',{'form':AuthenticationForm})
+        return render(request, 'login.html',{'form':AuthenticationForm})
     else:
         user = authenticate(request,
         username=request.POST['username'],
         password=request.POST['password'])
         if user is None:
-            return render(request,'loginaccount.html',{'form': AuthenticationForm(),'error': 'username and password do not match'})
+            return render(request,'login.html',{'form': AuthenticationForm(),'error': 'username and password do not match'})
         else:
             login(request,user)
             return redirect('home')
